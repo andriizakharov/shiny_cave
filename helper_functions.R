be938cd09372e47dd0a2cb885cf8e2289c9b5ab2
andriizakharov <khaozavr@gmail.com>
2018-05-30 16:36:52 +0200

    first ugly draft with cross-sectional sampling plots + some related helper functions

---
+++ helper_functions.R
@@ -0,0 +1,52 @@
+dat_transform <- function(data, sample_n, timepoints_n) {
+    dat_x <- as.data.frame(data[, 1:(sample_n/2)])
+    dat_y <- as.data.frame(data[, (sample_n/2+1):sample_n])
+    
+    names(dat_x) <- paste0("x", 1:timepoints_n)
+    names(dat_y) <- paste0("y", 1:timepoints_n)
+    
+    dat_x_long <- gather(dat_x)
+    dat_y_long <- gather(dat_y)
+    
+    dat_x_long$id <- rep(1:sample_n, timepoints_n)
+    dat_y_long$id <- rep(1:sample_n, timepoints_n)
+    
+    names(dat_x_long)[1] <- "timepoint"
+    names(dat_y_long)[1] <- "timepoint"
+    
+    dat_x_long$timepoint <- factor(rep(1:timepoints_n, each = sample_n))
+    dat_y_long$timepoint <- factor(rep(1:timepoints_n, each = sample_n))
+    
+    dat_long <- rbind(dat_x_long, dat_y_long)
+    dat_long$var <- factor(c(rep("x", nrow(dat_x_long)), 
+                             rep("y", nrow(dat_y_long))))
+    
+    dat_long
+    
+}
+
+dat_sample <- function(data, n_sample, seed = sample(0:100, 1)) {
+    set.seed(seed)
+    
+    dat_x_one_per_person <- data.frame(matrix(NA, nrow = n_sample, ncol = 3))
+    for (i in 1:n_sample) {
+        one_timepoint <- data[1:nrow(data)/2, ] %>% filter(id == i) %>% sample_n(1)
+        dat_x_one_per_person[i, ] <- one_timepoint
+    }
+    
+    dat_y_one_per_person <- data.frame(matrix(NA, nrow = n_sample, ncol = 3))
+    for (i in 1:n_sample) {
+        one_timepoint <- data[nrow(data)/2+1:nrow(data), ] %>% filter(id == i) %>% sample_n(1)
+        dat_y_one_per_person[i, ] <- one_timepoint
+    }
+    
+    names(dat_x_one_per_person) <- c("timepoint", "value", "id")
+    names(dat_y_one_per_person) <- c("timepoint", "value", "id")
+    
+    dat_one_per_person <- rbind(dat_x_one_per_person, dat_y_one_per_person)
+    dat_one_per_person$var <- factor(c(
+        rep("x", nrow(dat_one_per_person)/2), 
+        rep("y", nrow(dat_one_per_person)/2)))
+    
+    dat_one_per_person
+}
--- shiny_CAVE.R
+++ shiny_CAVE.R
@@ -1,132 +1,148 @@
 library(semper)
 source("bivariateLgcm.R")
 source("toOpenMx.R")
-source("input_presets.R")
+#source("input_presets.R")
+source("helper_functions.R")
 
 library(shiny)
 library(ggplot2)
 library(tidyverse)
 
-# Define UI for application that draws a histogram
+
 ui <- fluidPage(
     
     # Application title
     titlePanel("Bivariate LGCM simulation"),
     
-    # Sidebar with a slider input for number of bins 
+    # Sidebar
     sidebarLayout(
         sidebarPanel(
-            titlePanel("Presets:"),
+            h2("Presets:"),
             
             selectInput("preset",
                         "Choose a preset:",
-                        choices = list(
-                            Preset_1 = preset1,
-                            Preset_2 = preset2,
-                            Preset_3 = preset3
+                        choices = list("",
+                            "custom"
                         )),
             
-            titlePanel("Variable X:"),
+            conditionalPanel("input.preset == 'custom'",
+                h2("Variable X:"),
             
-            # Input params for variable X
-            numericInput("mean_icept_x",
-                         "X: intercept mean",
-                         value = 0),
-            numericInput("mean_slope_x",
-                         "X: slope mean",
-                         value = 0),
-            numericInput("var_icept_x",
-                         "X: intercept variance",
-                         value = 0),
-            numericInput("var_slope_x",
-                         "X: slope variance",
-                         value = 0),
-            numericInput("error_var_x",
-                         "X: error variance",
-                         value = 0),
-            numericInput("cov_icept_x_slope_x",
-                         "X: intercept-slope covariance",
-                         value = 0),
-            
-            titlePanel("Variable Y:"),
-            
-            # Input params for variable Y
-            numericInput("mean_icept_y",
-                         "Y: intercept mean",
-                         value = 0),
-            numericInput("mean_slope_y",
-                         "Y: slope mean",
-                         value = 0),
-            numericInput("var_icept_y",
-                         "Y: intercept variance",
-                         value = 0),
-            numericInput("var_slope_y",
-                         "Y: slope variance",
-                         value = 0),
-            numericInput("error_var_y",
-                         "Y: error variance",
-                         value = 0),
-            numericInput("cov_icept_y_slope_y",
-                         "Y: intercept-slope covariance",
-                         value = 0),
-            
-            titlePanel("Cross-variable:"),
-            
-            # Input intervariable params
-            numericInput("cov_icept_x_icept_y",
-                         "XY: intercept covariance",
-                         value = 0),
-            numericInput("cov_icept_x_slope_y",
-                         "XY: covariance intercept X - slope Y",
-                         value = 0),
-            numericInput("cov_icept_y_slope_x",
-                         "XY: covariance intercept Y - slope X",
-                         value = 0),
-            
-            sliderInput("cor_slope_x_slope_y",
-                        "XY: slope-slope correlation",
-                        min = 0,
-                        max = 1,
-                        value = 0.5),
-            
-            titlePanel("Other:"),
-            
-            # How many timepoints should the models simulate?
-            numericInput("timepoints",
-                         "Number of timepoints per model",
-                         min = 0,
-                         max = 100,
-                         value = 10),
-            
-            # How many simulated trajectories?
-            numericInput("sim_sample",
-                         "Number of simulated trajectories",
-                         min = 0,
-                         max = 100,
-                         value = 20),
-            
-            # Button to simulate data
-            actionButton("sim_data",
-                         "Simulate!")
+                # Input params for variable X
+                numericInput("mean_icept_x",
+                             "X: intercept mean",
+                             value = 0),
+                numericInput("mean_slope_x",
+                             "X: slope mean",
+                             value = 0),
+                numericInput("var_icept_x",
+                             "X: intercept variance",
+                             value = 0),
+                numericInput("var_slope_x",
+                             "X: slope variance",
+                             value = 0),
+                numericInput("error_var_x",
+                             "X: error variance",
+                             value = 0),
+                numericInput("cov_icept_x_slope_x",
+                             "X: intercept-slope covariance",
+                             value = 0),
+                
+                h2("Variable Y:"),
+                
+                # Input params for variable Y
+                numericInput("mean_icept_y",
+                             "Y: intercept mean",
+                             value = 0),
+                numericInput("mean_slope_y",
+                             "Y: slope mean",
+                             value = 0),
+                numericInput("var_icept_y",
+                             "Y: intercept variance",
+                             value = 0),
+                numericInput("var_slope_y",
+                             "Y: slope variance",
+                             value = 0),
+                numericInput("error_var_y",
+                             "Y: error variance",
+                             value = 0),
+                numericInput("cov_icept_y_slope_y",
+                             "Y: intercept-slope covariance",
+                             value = 0),
+                
+                h2("Cross-variable:"),
+                
+                # Input intervariable params
+                numericInput("cov_icept_x_icept_y",
+                             "XY: intercept covariance",
+                             value = 0),
+                numericInput("cov_icept_x_slope_y",
+                             "XY: covariance intercept X - slope Y",
+                             value = 0),
+                numericInput("cov_icept_y_slope_x",
+                             "XY: covariance intercept Y - slope X",
+                             value = 0),
+                
+                sliderInput("cor_slope_x_slope_y",
+                            "XY: slope-slope correlation",
+                            min = 0,
+                            max = 1,
+                            value = 0.5),
+                
+                h2("Other:"),
+                
+                # How many timepoints should the models simulate?
+                numericInput("timepoints",
+                             "Number of timepoints per model",
+                             min = 0,
+                             max = 100,
+                             value = 10),
+                
+                # How many simulated trajectories?
+                numericInput("sim_sample",
+                             "Number of simulated trajectories",
+                             min = 0,
+                             max = 100,
+                             value = 20),
+                
+                # Button to simulate data
+                actionButton("sim_data",
+                             "Simulate!"),
+                
+                # Set seed for sampling
+                numericInput("seed",
+                             "Set random seed for sampling",
+                             min = -100,
+                             max = 100,
+                             value = 42),
+                
+                # Button to draw a cross-sectional sample
+                actionButton("sample",
+                             "Sample cross-section")
+            )
         ),
         
         # Show 
         mainPanel(
             titlePanel("SOS:"),
-            htmlOutput("sos_val"),
-            
-            plotOutput("lgcm_graphs")
+            h2(htmlOutput("sos_val")),
+            plotOutput("lgcm_graphs"),
+            plotOutput("scatter_plots")
         )
     )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
 
-    output$sos_val <- renderText({
-        
-        # Define the models given all input parameters
-        lgcm_x <- lgcm(
+server <- function(input, output) {
+    
+    ### Reactive calculations
+    
+    # Define the models given all input parameters
+    lgcm_x <- reactive({ 
+        req(input$timepoints, input$var_icept_x, input$var_slope_x,
+            input$error_var_x, input$cov_icept_x_slope_x, input$mean_icept_x,
+            input$mean_slope_x)
+        lgcm(
             timepoints = 1:input$timepoints,
             intercept.variance = input$var_icept_x,
             slope.variance = input$var_slope_x,
@@ -134,8 +150,12 @@ server <- function(input, output) {
             intercept.slope.covariance = input$cov_icept_x_slope_x,
             intercept.mean = input$mean_icept_x,
             slope.mean = input$mean_slope_x
-        )
-        lgcm_y <- lgcm(
+    ) })
+    lgcm_y <- reactive({ 
+        req(input$timepoints, input$var_icept_y, input$var_slope_y,
+            input$error_var_y, input$cov_icept_y_slope_y, input$mean_icept_y,
+            input$mean_slope_y)
+        lgcm(
             timepoints = 1:input$timepoints,
             intercept.variance = input$var_icept_y,
             slope.variance = input$var_slope_y,
@@ -143,73 +163,81 @@ server <- function(input, output) {
             intercept.slope.covariance = input$cov_icept_y_slope_y,
             intercept.mean = input$mean_icept_y,
             slope.mean = input$mean_slope_y
-        )
-        lgcm_xy <- bivariateLgcm(
-            lgcm_x, lgcm_y, 
-            icept.x.icept.y.covariance = input$cov_icept_x_icept_y, 
-            slope.x.slope.y.covariance = input$cov_slope_x_slope_y
-        )
-        
-        # Calculate some values
-        cov_slope_x_slope_y <- input$cor_slope_x_slope_y * 
-            sqrt(input$var_slope_x * input$var_slope_y)
-        
-        var_x <- input$var_icept_x + 1/3*input$var_slope_x + input$cov_icept_x_slope_x + 
-            1/12*input$mean_slope_x**2 + input$error_var_x
-        var_y <- input$var_icept_y + 1/3*input$var_slope_y + input$cov_icept_y_slope_y + 
-            1/12*input$mean_slope_y**2 + input$error_var_y
-        
-        cov_x_y <- input$cov_icept_x_icept_y + 
-            1/2*(input$cov_icept_x_slope_y + input$cov_icept_y_slope_x) +
-            1/3*cov_slope_x_slope_y +
-            1/12*input$mean_slope_x*input$mean_slope_y
-        
-        # Compute SOS
-        sos <- 1 - 12*var_y*(input$mean_slope_y*var_x - input$mean_slope_x*cov_x_y)**2 /
-            ((var_y*var_x - cov_x_y**2) * (12*var_x - input$mean_slope_x**2)*input$mean_slope_y**2)
-        
-        # And show it
-        sos
+    ) })
+    
+    cov_slope_x_slope_y <- reactive({ 
+        req(input$cor_slope_x_slope_y, input$var_slope_x, input$var_slope_y)
+        input$cor_slope_x_slope_y * sqrt(input$var_slope_x * input$var_slope_y) 
     })
     
+    lgcm_xy <- reactive({ 
+        req(input$cov_icept_x_icept_y)
+        bivariateLgcm(
+            lgcm_x(), lgcm_y(), 
+            icept.x.icept.y.covariance = input$cov_icept_x_icept_y, 
+            slope.x.slope.y.covariance = cov_slope_x_slope_y()
+        ) })
+    
+    var_x <- reactive({ 
+        req(input$var_icept_x, input$var_slope_x, input$cov_icept_x_slope_x,
+            input$mean_slope_x, input$error_var_x)
+        input$var_icept_x + 1/3*input$var_slope_x + input$cov_icept_x_slope_x + 
+        1/12*input$mean_slope_x**2 + input$error_var_x })
+    var_y <- reactive({ 
+        req(input$var_icept_y, input$var_slope_y, input$cov_icept_y_slope_y,
+            input$mean_slope_y, input$error_var_y)
+        input$var_icept_y + 1/3*input$var_slope_y + input$cov_icept_y_slope_y + 
+        1/12*input$mean_slope_y**2 + input$error_var_y })
+    
+    cov_x_y <- reactive({ 
+        req(input$cov_icept_x_icept_y, input$cov_icept_x_slope_y, 
+            input$cov_icept_y_slope_x, input$mean_slope_x, input$mean_slope_y)
+        input$cov_icept_x_icept_y + 
+        1/2*(input$cov_icept_x_slope_y + input$cov_icept_y_slope_x) +
+        1/3*cov_slope_x_slope_y() +
+        1/12*input$mean_slope_x*input$mean_slope_y })
+    
+    # Compute SOS
+    sos <- reactive({ 
+        req(input$mean_slope_y, input$mean_slope_x)
+        1 - 12*var_y()*(input$mean_slope_y*var_x() - input$mean_slope_x*cov_x_y())**2 /
+        ((var_y()*var_x() - cov_x_y()**2) * (12*var_x() - input$mean_slope_x**2)*
+             input$mean_slope_y**2) })
+    
+    # Make an OpenMx model
+    mx_mod <- reactive({ toOpenMx(lgcm_xy()) })
+    
+    # Simulate data if button is clicked
+    dat_long <- eventReactive(input$sim_data, {
+        req(input$sim_sample, input$timepoints)
+        dat_transform(simulateData(mx_mod(), input$sim_sample), 
+                      input$sim_sample, input$timepoints)
+    })
+    
+    dat_one_per_person <- eventReactive(input$sample, {
+        dat_sample(dat_long(), input$sim_sample, seed = input$seed)
+    })
+    
+    output$sos_val <- renderText( sos() )
+    
     output$lgcm_graphs <- renderPlot({
-        if (input$sim_data == TRUE) {
-            # Make an OpenMx model
-            mx_mod <- toOpenMx(lgcm_xy)
-            
-            # Simulate data
-            dat <- simulateData(mx_mod, input$sim_sample)
-            
-            dat_x <- as.data.frame(dat[, 1:(input$sim_sample/2)])
-            dat_y <- as.data.frame(dat[, (input$sim_sample/2+1):input$sim_sample])
-            
-            names(dat_x) <- paste0("x", 1:input$timepoints)
-            names(dat_y) <- paste0("y", 1:input$timepoints)
-            
-            dat_x_long <- gather(dat_x)
-            dat_y_long <- gather(dat_y)
-            
-            dat_x_long$id <- rep(1:input$sim_sample, input$timepoints)
-            dat_y_long$id <- rep(1:input$sim_sample, input$timepoints)
-            
-            names(dat_x_long)[1] <- "timepoint"
-            names(dat_y_long)[1] <- "timepoint"
-            
-            dat_x_long$timepoint <- factor(rep(1:input$timepoints, each = input$sim_sample))
-            dat_y_long$timepoint <- factor(rep(1:input$timepoints, each = input$sim_sample))
-            
-            dat_long <- rbind(dat_x_long, dat_y_long)
-            dat_long$var <- factor(c(rep("x", nrow(dat_x_long)), 
-                                     rep("y", nrow(dat_y_long))))
-            
-            plt_x_y <- ggplot(dat_long, aes(x = timepoint, y = value)) +
-                geom_line(aes(group = id)) +
-                facet_grid(var ~.)
-            
-            plt_x_y
-        }
+        
+        ggplot(dat_long(), aes(x = timepoint, y = value)) +
+            geom_line(aes(group = id)) +
+            facet_grid(var ~.)
+        
+    })
+    
+    output$scatter_plots <- renderPlot({
+        
+        ggplot(dat_one_per_person(), aes(x = timepoint, y = value)) +
+            geom_point() +
+            facet_grid(var ~.)
     })
 }
+
+
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
